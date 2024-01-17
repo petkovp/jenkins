a1f9705689af8fb9f4ddeaaf795b89d118a8ae42
petko.petkov <petko.petkov@egt-digital.com>
2024-01-17 11:13:30 +0200

    Added fist-pipeline groovy

---
+++ jobs/pipeline/first-pipeline.groovy
@@ -0,0 +1,18 @@
+pipelineJob('First-Pipeline') {
+    description('Pipeline Job')
+    displayName('First-Pipeline')
+    definition {
+        cpsScm {
+          scm {
+            git {
+                remote {
+                    name('Jenkins jobs')
+                    url('https://github.com/petkovp/jenkins.git')
+                }
+                branch('main')
+            }
+          }
+          scriptPath("Jenkins")
+      }
+    }
+ }
