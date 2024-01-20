1d2c3c9986899e7f4d7965137e17be0dab26ef71
petko.petkov <petko.petkov@egt-digital.com>
2024-01-20 20:22:45 +0200

    Added new multiBranch-AWS

---
+++ jobs/pipeline/multiBranch-AWS.groovy
@@ -0,0 +1,20 @@
+multibranchPipelineJob('Payment-Gateway/MultiBranch-AWS') {
+    description('MultiBranch AWS Pipeline Job')
+    displayName('MultiBranch-Pipeline-AWS')
+    branchSources {
+        git {
+            id('multibranchAWSPipelineJob')
+            remote('https://github.com/petkovp/MultiBranch-AWS.git')
+
+        }
+    }
+     factory {
+        workflowBranchProjectFactory {
+        scriptPath('Jenkinsfile')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+    }
+  }
+}
