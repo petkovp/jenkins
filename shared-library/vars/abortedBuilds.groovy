431d3e2f408568a3d551fca2b3f9cf36d4a58974
petko.petkov <petko.petkov@egt-digital.com>
2024-05-12 00:23:22 +0300

    Tested new features

---
+++ shared-library/vars/abortedBuilds.groovy
@@ -0,0 +1,26 @@
+Jenkins.instance.queue.items.findAll { !it.task.name.contains("Extenda") }.each {
+  println "Cancel ${it.task.name}"
+  Jenkins.instance.queue.cancel(it.task)
+}
+Jenkins.instance.items.each {
+  stopJobs(it)
+}
+def stopJobs(job) {
+  if (job in jenkins.branch.OrganizationFolder) {
+    // Git behaves well so no need to traverse it.
+    return
+  } else if (job in com.cloudbees.hudson.plugins.folder.Folder) {
+    job.items.each { stopJobs(it) }
+  } else if (job in org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject) {
+    job.items.each { stopJobs(it) }
+  } else if (job in org.jenkinsci.plugins.workflow.job.WorkflowJob) {
+    if (job.isBuilding() || job.isInQueue() || job.isBuildBlocked()) {
+      job.builds.findAll { it.inProgress || it.building }.each { build ->
+        println "Kill $build"
+        build.finish(hudson.model.Result.ABORTED, new java.io.IOException("Aborted from Script Console"));
+      }
+    }
+  }
+}
+
+return true
