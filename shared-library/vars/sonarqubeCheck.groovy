c728eec85150438817a923f5e1cda2f6b0d5145b
petko.petkov <petko.petkov@egt-digital.com>
2024-05-13 17:34:20 +0300

    Tested new features

--- shared-library/vars/sonarqubeCheck.groovy
+++ shared-library/vars/sonarqubeCheck.groovy
@@ -1,5 +1,14 @@
+import hudson.model.*
+import jenkins.model.*
+import hudson.plugins.sonar.*
+import hudson.plugins.sonar.model.TriggersConfig
+import hudson.tools.*
+
+def sonarqube = "SonarQube"
+def sonarqubeId = "sonarqube"
+
+
 def call() {
     sh 'mvn clean package sonar:sonar'
-    withSonarQubeEnv(installationName: 'SonarQube', credentialsId: 'sonarqube')
     waitForQualityGate abortPipeline: false
 }
