f360606b9f13ef88021a8ad0e99cdf25706c890c
petko.petkov <petko.petkov@egt-digital.com>
2024-05-13 19:00:55 +0300

    Tested new features

--- shared-library/vars/qualityGateCheck.groovy
+++ shared-library/vars/qualityGateCheck.groovy
@@ -1,4 +1,27 @@
-def qualitygate = waitForQualityGate()
-if (qualitygate.status != "OK") {
-    error "Pipeline aborted due to quality gate coverage failure: ${qualitygate.status}"
+@Grapes(
+    @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1')
+)
+import static groovyx.net.http.ContentType.TEXT
+// Define URL variable
+String taskURL = "http://<SONAR_URL>/api/ce/task?id=TASK_ID"
+String projectStatusURL = "http://<SONAR_URL>/api/qualitygates/project_status?analysisId="
+
+// Get project status
+def status=taskURL(taskURL).task.status
+while ( status == "PENDING" || status == "IN_PROGRESS" ) {
+println "waiting for sonar results"
+status = httpClient(taskURL).task.status
+sleep(1000)
 }
+      assert status != "CANCELED" : "Build fail because sonar project is CANCELED"
+      assert status != "FAILED" : "Build fail because sonar project is FAILED"
+      def qualitygates= httpClient(projectStatusURL + httpClient(taskURL).task.analysisId)
+      assert qualitygates.projectStatus.status != "ERROR" : "Build fail because sonar project status is not ok"
+      println "Huraaaah! You made it :) Sonar Results are good"
+
+def httpClient(String url){
+    def taskClient = new groovyx.net.http.HTTPBuilder(url)
+    taskClient.setHeaders(Accept: 'application/json')
+    def response =  taskClient.get(contentType: TEXT)
+    def sluper = def new groovy.json.JsonSlurper().parse(response)
+}  
